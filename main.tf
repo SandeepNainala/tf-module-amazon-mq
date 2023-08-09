e098dd0da64216a61544d685c313b9e82a786b32
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-08-09 17:35:28 +0530

    Terraform - roboshop - elasticache - security group, replication group

--- main.tf
+++ main.tf
@@ -31,14 +31,14 @@ resource "aws_security_group" "main" {
 
 
 resource "aws_instance" "rabbitmq" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  subnet_id = var.subnets[0]
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnets[0]
   vpc_security_group_ids = [aws_security_group.main.id]
-  tags = merge(var.tags,{ Name = "${var.name}-${var.env}"})
+  tags                   = merge(var.tags, { Name = "${var.name}-${var.env}" })
 
   root_block_device {
-    encrypted = true
+    encrypted  = true
     kms_key_id = var.kms_arn
   }
 
